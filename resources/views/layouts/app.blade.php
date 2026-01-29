c19af921db8c6850fb06408d8ef394cbbbd16e10
Valentim Verardo <vb.verardo@gmail.com>
2026-01-28 23:00:40 -0300

    more and more changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <link rel="icon" href="{{ asset('/images/favicon.png') }}" type="image/x-icon">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('/images/favicon.jpg')}}"/>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
