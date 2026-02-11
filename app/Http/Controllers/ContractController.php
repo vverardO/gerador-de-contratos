e195da8150df5d07e890ca3b2a78498b7009e1e3
Valentim Verardo <vb.verardo@gmail.com>
2026-02-10 23:04:26 -0300

    added logo on the bottom of the contract

--- app/Http/Controllers/ContractController.php
+++ app/Http/Controllers/ContractController.php
@@ -53,7 +53,15 @@ class ContractController extends Controller
         $contract = Contract::findOrFail($id);
         $html = $this->contractService->getContractHtml($contract);
 
-        $pdf = Pdf::loadHTML($html);
+        $footerCss = '@page { margin-bottom: 70px; }'
+            .'.pdf-footer { position: fixed; left: 0; right: 10px; bottom: 10px; height: 50px; text-align: right; z-index: 9999; }'
+            .'.pdf-footer img { height: 45px; width: auto; filter: grayscale(100%); }';
+        $footerHtml = '<div class="pdf-footer"><img src="images/watter_mark.jpg" alt=""></div>';
+
+        $html = str_replace('</head>', '<style>'.$footerCss.'</style></head>', $html);
+        $html = str_replace('</body>', $footerHtml.'</body>', $html);
+
+        $pdf = Pdf::loadHTML($html)->setBasePath(public_path());
 
         $driverName = strtoupper(preg_replace('/[^A-Z0-9]/i', '-', $contract->driver_name));
 
